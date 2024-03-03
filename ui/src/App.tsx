6de4d677eb21fbd7f571a846d27bbd589f13eea7
Ana Levit <anaalamed@gmail.com>
2024-03-03 14:54:40 +0200

    UI: create basic layout

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout } from "antd";
+import { GlobalStyles } from "./styles/global";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Header, Footer, Content } = Layout;
+
+const headerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundImage: "linear-gradient(180deg, #532ac6 0%, #1d1674 100%)",
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#000",
+  backgroundColor: "#fff",
+};
+
+const footerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  backgroundImage: "linear-gradient(180deg, #532ac6 0%, #1d1674 100%)",
+};
+
+const layoutStyle = {
+  overflow: "hidden",
+  width: "100%",
+  minHeight: "100vh",
+};
+
+const App: React.FC = () => (
+  <Layout style={layoutStyle}>
+    <Header style={headerStyle}>Header</Header>
+    <Content style={contentStyle}>Content</Content>
+    <Footer style={footerStyle}>Footer</Footer>
+
+    <GlobalStyles />
+  </Layout>
+);
 
 export default App;
